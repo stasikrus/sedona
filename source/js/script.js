486bfe3e860cf2a9c2529fae78be5ab645cf5413
stasikrus <stas-boss@mail.ru>
2022-12-07 18:40:49 +0300

    update

---
+++ source/js/script.js
@@ -0,0 +1,14 @@
+let menuButton = document.querySelector('.main-nav_toggle');
+let navMenu = document.querySelector('.main-nav');
+
+navMenu.classList.remove('main-nav--nojs');
+
+menuButton.addEventListener('click', function() {
+  if (navMenu.classList.contains('main-nav--closed')) {
+    navMenu.classList.remove('main-nav--closed');
+    navMenu.classList.add('main-nav--opened');
+  } else {
+    navMenu.classList.add('main-nav--closed');
+    navMenu.classList.remove('main-nav--opened');
+  }
+})
