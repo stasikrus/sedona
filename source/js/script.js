c5a4ca2e1a44cad229635415a1ddd04ed17ee771
stasikrus <stas-boss@mail.ru>
2022-12-08 16:38:45 +0300

    update

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,8 @@
 let menuButton = document.querySelector('.main-nav_toggle');
 let navMenu = document.querySelector('.main-nav');
 
-navMenu.classList.remove('main-nav--nojs');
+navMenu.classList.remove('main-nav--opened');
+navMenu.classList.add('main-nav--closed');
 
 menuButton.addEventListener('click', function() {
   if (navMenu.classList.contains('main-nav--closed')) {
